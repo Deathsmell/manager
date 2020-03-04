858d2b8879e9a2a3e8be307f2840ac316f619ced
john <xelasel.rotmen2010@gmail.com>
2020-03-04 19:11:41 +0300

    Initial commit

---
+++ src/main/java/com/steis/manager/ManagerApplication.java
@@ -0,0 +1,13 @@
+package com.steis.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApplication.class, args);
+    }
+
+}
