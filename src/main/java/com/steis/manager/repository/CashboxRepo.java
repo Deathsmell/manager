8779883b309e993926afbfcd3adbaa2b22cd5401
john <xelasel.rotmen2010@gmail.com>
2020-03-27 19:44:48 +0300

    Fixed n+1 and queries. Add order by in all queries

--- src/main/java/com/steis/manager/repository/CashboxRepo.java
+++ src/main/java/com/steis/manager/repository/CashboxRepo.java
@@ -2,26 +2,36 @@ package com.steis.manager.repository;
 
 import com.steis.manager.domain.Cashbox;
 import com.steis.manager.domain.Client;
+import com.steis.manager.domain.Master;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Set;
 
 @Repository
 public interface CashboxRepo extends JpaRepository<Cashbox, Long> {
 
+    @EntityGraph(attributePaths = { "client" })
+    List<Cashbox> findAllByOrderByAddress();
+
     List<Cashbox> findByClient(Client client);
 
-    @Query(value = "select cb from Client cl inner join Cashbox cb on cl.id=cb.client.id where upper(cb.address) like %?1%")
-    List<Cashbox> findAllByAddressContaining(String address);
+    @EntityGraph(attributePaths = { "client" })
+    @Query(value = "select cb from Cashbox cb where upper(cb.address) like %:address% order by cb.address asc")
+    List<Cashbox> findAllByAddressContaining(@Param("address") String address);
 
-    @Query(value = "select cb from Client cl inner join Cashbox cb on cl.id=cb.client.id where upper(cl.name) like %?1%")
+    @EntityGraph(attributePaths = { "client" })
+    @Query(value = "select cb from Cashbox cb where upper(cb.client.name) like %:name% order by cb.address asc")
     List<Cashbox> findAllByClientName(@Param("name") String name);
 
-    @Query(value = "select cb from Client cl inner join Cashbox cb on cl.id=cb.client.id where upper(cl.name) like %?1% and lower(cb.address) like %?2%")
+    @EntityGraph(attributePaths = { "client" })
+    @Query(value = "select cb from Cashbox cb where upper(cb.client.name) like %:name% and upper(cb.address) like %:address% order by cb.address asc")
     List<Cashbox> findAllByClientNameAndAddress(@Param("name") String name, @Param("address") String address);
 
+    @EntityGraph(attributePaths = { "client" })
+    List<Cashbox> findByMaster(Master master);
 }
